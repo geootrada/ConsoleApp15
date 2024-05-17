5d2818dee24b53b61be7df795c31abb039f73d7b
geootrada <geootrada@yandex.ru>
2024-05-17 14:47:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int sum = 0;
+
+            Random rand = new Random();
+            number = rand.Next(0, 100);
+            Console.WriteLine($"Мы получили случайное число {number}.");
+
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                sum += i;
+            }
+            Console.WriteLine($"Полученная сумма по заданию {sum}");
+        }
+    }
+}
